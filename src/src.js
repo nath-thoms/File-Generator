4d20b0388910cce61a5080613650751d36d45df6
Nath Chadwick <nath.chadwick@gmail.com>
2018-03-07 15:26:54 +0000

    initial functionality for writing files/folders

--- src/src.js
+++ src/src.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+const fs = require('fs');
+
+
+function newProject() {
+    fs.mkdir('../test', (err, res) => {
+        if (err) return console.log(err);
+        fs.writeFile('../test/index.js', 'Hello', 'utf8', (err, res) => {
+            if (err) return console.log(err);
+            fs.mkdir('../test/spec', (err, res) => {
+                if (err) return console.log(err);
+                fs.writeFile('../test/spec/index.spec.js', 'Spec', 'utf8', (err, res) => {
+
+                })
+            })
+        })
+    });
+}
+newProject();
